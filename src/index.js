40c094fd9426ff9647c21f37af482e5b2a0e7124
Steven Otalvaro <fabiosteven1998.12@gmail.com>
2020-09-23 11:56:30 -0500

    Creacion del Html Todo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import './styles.css';
 import { Todo } from './classes/todo.class.js';
 import {TodoList} from './classes/todo-list-class.js';
+import {crearTodoHtml} from './js/componentes.js';
 
 const todoList = new TodoList();
 
 const tarea = new Todo('Javascript');
-const tarea2 = new Todo('PHP');
-
 todoList.nuevoTodo(tarea);
-todoList.nuevoTodo(tarea2);
 
 console.log(todoList);
+// tarea.completado = true;
+
+crearTodoHtml(tarea);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,14 +1,23 @@
-import '../css/componentes.css';
+// referencias en el html
 
+const divTodoList = document.querySelector('.todo-list');
 
+export const crearTodoHtml = (todo) =>{
 
-export const saludar = ( nombre ) => {
+    const htmlTodo = `
+    <li class="${ (todo.completado) ? 'completed' : ''}" data-id="${todo.id}">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${(todo.completado) ? 'checked' : '' }>
+            <label>${todo.tarea}</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    </li> `;
 
-    console.log('Creando etiqueta h1, en el HTML!');
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${ nombre }`;
-
-    document.body.append( h1 );
-
+    divTodoList.append(div.firstElementChild);
+    return div.firstElementChild;
 }
+
